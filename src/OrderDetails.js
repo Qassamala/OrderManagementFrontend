830bd8514aebda65e0bfa21842b0d406b5175bfc
Qassamala <abdigobena@hotmail.com>
2020-03-25 15:02:28 +0100

    id is now sent in newOrderData

--- src/OrderDetails.js
+++ src/OrderDetails.js
@@ -9,22 +9,30 @@ class OrderDetails extends Component {
       super(props);
       this.state = {
           products: [],
-          orderRows: []
+          orderRows: [],
+          newOrderData: {
+            id: null,
+            customerId: '',
+            totalSum: '',
+            totalDiscount: '',
+            rows: []
+        },
       }
       this.editOrder = this.editOrder.bind(this);
       this.deleteOrder = this.deleteOrder.bind(this);
+      this._refreshOrdersList = this._refreshOrdersList.bind(this);
+
 
     //   this.detailsOrder = this.detailsOrder.bind(this);
 
     }
     async componentDidMount(){
-        console.log(this.props.orderId)
         await Axios.get('https://localhost:44345/api/Products').then((response) =>{
            this.setState({
             products: response.data,
           })
         });
-        console.log(this.state.products)
+
         await Axios.get('https://localhost:44345/api/Orders/OrderRows/' + this.props.orderId).then((response) =>{
           console.log(response.data)      
           this.setState({
@@ -39,11 +47,96 @@ class OrderDetails extends Component {
        });
      }
 
-     deleteOrder(id){
-        Axios.delete('https://localhost:44345/api/Orders/' + id)
+     updateOrder(){
+        console.log(this.state.newOrderData)
+        Axios.put('https://localhost:44345/api/Orders/OrderRow/' + this.state.newOrderData.orderId, this.state.newOrderData)
         .then((response) => {
+          this._refreshOrdersList();          
+      });
+      this.setState({
+        newOrderData: {
+         orderId: null,
+         customerId: '',
+         totalSum: '',
+         totalDiscount: '',
+         rows: []
+      }});
+       
+    }
+    
+    async _refreshOrdersList(){
+        console.log(this.state.orderRows)
+      
+        await Axios.get('https://localhost:44345/api/Orders/OrderRows/' + this.props.orderId).then((response) =>{
+            console.log(response.data)      
+            this.setState({
+              orderRows: response.data
+            })
+          }); 
+        }
+
+     async deleteOrder(rowId){
+         
+        //Calculate new total sum and total discount
+        const rows = Object.assign([], this.state.orderRows);
+
+        console.log(rows);
+
+
+        var index = rows.map(x => {
+            return x.Id;
+          }).indexOf(rowId);
+
+        rows.splice(index, 1);
+
+        console.log(this.state.orderRows);
+
+        console.log(rows);
+
+
+        let totalSum = null;
+        rows.forEach(row => totalSum += row.totalSum)
+
+        let totalDiscount = null;
+        rows.forEach(row => totalDiscount += row.totalDiscount)
+
+        console.log(this.props.orderId)
+
+        await this.setState(prevState => ({
+            newOrderData: {
+              ...prevState.newOrderData,
+              id: this.props.orderId,
+              customerId: this.context.customer.id,
+              totalSum: totalSum,
+              totalDiscount: totalDiscount,
+              rows : rows
+            }
+          }));
+
+        console.log(rowId)
+        console.log(this.state.newOrderData)
+
+        Axios.put('https://localhost:44345/api/Orders/' + this.state.newOrderData.id, this.state.newOrderData)
+        .then((response) => {
+            console.log(response);
           this._refreshOrdersList();
-      }); 
+      });
+
+      this.setState({
+        newOrderData: {
+         id: null,
+         customerId: '',
+         totalSum: '',
+         totalDiscount: '',
+         rows: []
+      }});
+
+        //   this.updateOrder();
+
+    //     Axios.delete('https://localhost:44345/api/Orders/' + id)
+    //     .then((response) => {
+    //       this._refreshOrdersList();
+    //   }); 
     }
     
   
